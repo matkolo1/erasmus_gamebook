6f2e0b3363bc637c6d4d3a885833e0dc4701d3ba
matkolo1 <matej.kor@email.cz>
2026-02-04 13:23:27 +0100

    finishing touches

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ let textField = document.getElementById("textHere");
 let buttonField = document.getElementById("buttonsHere");
 let textFileContent = getTextFile().split("/").slice(1);
 let currentFrame = (Number)(new URLSearchParams(window.location.search).get("frame"));
+if (currentFrame == 0) currentFrame = 1
 
 
 function getTextFile() {
@@ -35,16 +36,17 @@ class textByFrame {
     }
 
     splitFrameText() {
-        this.frameText = this.origText.split(" ").slice(1).join(" ").split("-")[0];
+        this.frameText = this.origText.split(" ").slice(1).join(" ").split(/-\d{3}/)[0];
     }
 
     splitButtons() {
         this.btnText = []
-        let tempText = this.origText.split("-").slice(1);
-        for (let btn of tempText) {
+        let tempText = this.origText.split(/-(\d{3})/).slice(1);
+        console.log(tempText)
+        for (let i = 0; i < tempText.length; i += 2) {
             let tempBTN = [];
-            tempBTN.push(btn.split(" ")[0]);
-            tempBTN.push(btn.split(" ").slice(1).join(" "));
+            tempBTN.push(tempText[i]);
+            tempBTN.push(tempText[i + 1]);
             this.btnText.push(tempBTN);
         }
 
@@ -57,7 +59,7 @@ class textByFrame {
         let tempButtons = ``;
         if (!this.btnText) return;
         for (let btn of this.btnText) {
-            tempButtons += `<button onclick="move(${btn[0]})">${btn[1]}</button>`;
+            tempButtons += `<button onclick="move(${btn[0].toString().slice(1)})">${btn[1].trim()}</button>`;
         }
         console.table(this.btnText)
         console.log(tempButtons)
@@ -69,5 +71,6 @@ let frame = new textByFrame(textFileContent[currentFrame - 1]);
 frame.printFrame()
 frame.printButtons()
 function move(num) {
+    console.log(num)
     window.location.href = window.location.origin + window.location.pathname + "?frame=" + num;
 }
